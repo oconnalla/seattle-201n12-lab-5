42e0c0d4e194f3abb86498818a68f1b4e06e769b
oconnalla <oconnalla@gmail.com>
2018-09-22 14:35:15 -0700

    problem3 solved

--- app.js
+++ app.js
@@ -6,14 +6,14 @@ Write a function called sum() that takes in two numbers as arguments and then re
 "The sum of 4 and 7 is 11."
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testSum() function below and check the console to see if the test passes.*/
+// var altMessage = ('The sum of ' + a + ' and ' + b + ' is ' + solution1 + '.');
 // Write your code here
 
 function sum(a, b) { //eslint-disable-line
   var solution1 = a + b;
-  var solution1Message= ('The sum of ' + a + ' and ' + b + ' is ' + solution1 + '.');
+  var solution1Message= `The sum of ${a} and ${b} is ${solution1}.`;
   return [solution1,solution1Message];
 }
-
 
 // Here is the test for sum(); uncomment it to run it
 testSum(4, 7);
@@ -26,20 +26,19 @@ Write a function called multiply() that takes in two numbers as arguments and re
 "The product of 5 and 9 is 45."
 
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
+// ('The product of ' + a + ' and ' + b + ' is ' + math2 + '.');
+// console.log(math2, math2message);
 
 // Write your code here
 function multiply(a, b) { //eslint-disable-line
   var math2 = a * b ;
-  var math2message = ('The product of ' + a + ' and ' + b + ' is ' + math2 + '.');
-  console.log(math2, math2message);
+  var math2message = `The product of ${a} and ${b} is ${math2}.`;
   return[math2, math2message];
 }
 
 // Here is the test for multiply(); uncomment it to run it
 testMultiply(5, 9);
 
-// Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
@@ -47,8 +46,8 @@ testMultiply(5, 9);
 /* Problem 3
 Write a function called sumAndMultiply() that takes in three numbers as separate arguments and returns an array where the first element is the sum of those three numbers, the second element is the product of those three numbers,  and the third and fourth elements are strings that EXACTLY follow this example and use the values that were input into the function:
 
-Third element: "4 and 7 and 5 sum to 16."
-Fourth element: "The product of 4 and 7 and 5 is 140."
+// Third element: "4 and 7 and 5 sum to 16."
+// Fourth element: "The product of 4 and 7 and 5 is 140."
 
 IMPORTANT DETAIL: You may not use the arithmetic operators + and * in this function. To do addition, use your sum() function, and to do multiplication, use your multiply() function that you've already created. You're going to have to be resourceful to figure out how to do this.
 
@@ -56,11 +55,24 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sumAndMultiply(a, b, c) { //eslint-disable-line
+  var sum3Var = sum(sum(a, b)[0],c)[0];
+  var math3SumMessage = `${a} and ${b} and ${c} sum to ${sum3Var}.`;
+  console.log(sum3Var);
+  console.log (math3SumMessage);
 
+  var product3Var = multiply(multiply(a,b)[0],c)[0];
+  var math3ProductMessage = `The product of ${a} and ${b} and ${c} is ${product3Var}.`;
+  console.log(product3Var);
+  console.log(math3ProductMessage);
+  
+  console.log(sumAndMultiply);
+  return[sum3Var, product3Var, math3SumMessage, math3ProductMessage];
 }
-
-// Here is the test for sumAndMultiply(); uncomment it to run it
-// testSumAndMultiply(4,7,5);
+// array[]
+// return[sum3Var, product3Var, math3SumMessage, mathProductMessage];
+// sum[0] +  sum(c)//at position in array
+// // Here is the test for sumAndMultiply(); uncomment it to run it
+testSumAndMultiply(4,7,5);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
